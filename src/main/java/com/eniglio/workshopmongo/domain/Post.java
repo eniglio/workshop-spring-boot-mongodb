e56d688d9d6d17db6e323e83b798466755fadf57
eniglio <eniglio@hotmail.com>
2025-07-16 20:25:14 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/eniglio/workshopmongo/domain/Post.java
+++ src/main/java/com/eniglio/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.eniglio.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.eniglio.workshopmongo.dto.AuthorDTO;
+import com.eniglio.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,8 @@ public class Post implements Serializable{
 	public String title;
 	public String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -73,6 +78,14 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,5 +103,4 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
