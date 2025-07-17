7b198f68073b558e822322b10db4cdffb83c3a3c
eniglio <eniglio@hotmail.com>
2025-07-16 21:15:35 -0300

    Consulta simples com @Query

--- src/main/java/com/eniglio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/eniglio/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.eniglio.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.eniglio.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.eniglio.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	//Spring Data mongoDB - consultar no site
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
